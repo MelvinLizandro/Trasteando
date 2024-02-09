0b863c8346bee556efac28bc0f3a3167b9b70d9d
MelvinLizandro <melvinrivas@unitec.edu>
2024-02-09 12:00:27 -0600

    hola

---
+++ aaaaaa/src/aaaaaa/Aaaaaa.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package aaaaaa;
+
+/**
+ *
+ * @author l3306
+ */
+public class Aaaaaa {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
